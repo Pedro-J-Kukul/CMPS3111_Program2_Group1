a4beb8b218cf7a217f461095055ceb043624d70e
Pedro-Kukul <pedro.j.kukul@gmail.com>
2024-10-30 17:26:47 -0600

    Julius Fix This

--- parsing_tree.jl
+++ parsing_tree.jl
@@ -1,103 +1,70 @@
-# Function to print the top part of the parse tree
-function topPart()
-    top = ["[program]", "wake", "[assignments]", "sleep"]
-    println("                             $(top[1])")
-    println("                       /          |        \\     ")
-    println("                    $(top[2])   $(top[3])  $(top[4])")
-    println("                             /        \\")
-end
+function parseTree(state::DerivationState)
+    assignments = state.validKeys
+    if length(assignments) == 1
 
-# Function to print the parse tree based on verified input
-function parseTree(input::AbstractString)
-    # Splitting by semicolons to identify key assignments
-    inputF = split(input, ";")
-    inputF = strip.(inputF)  # Strip whitespace from each element
+        # tokens will be in key [key] = [drive]
+        # use a dummy array for testing
+        tokens = split(assignments[1], r"\s+")
+        tree = """
+                                          [program]
+                                      /       |       \\
+                                 wake   [assignment-list]   sleep
+                                              |
+                                       [key-assignment]
+                                              |
+                                     key [key] = [command]
+                                            /        \\
+                                           $(tokens[2])      [command]
+                                                       \\
+                                                        $(tokens[4])
+               """
+    elseif length(assignments) == 2
+        tokens1 = split(assignments[1], r"\s+")
+        tree = """
+                                          [program]
+                                      /       |       \\
+                                 wake   [assignment-list]   sleep
+                                    /                       |
+                       [key-assignment]                   [assignment-list]
+                    /                     \\
+                     key [key] = [command]
+                            /        \\
+                           $(tokens[2])      [command]
+                                       \\
+                                        $(tokens[4])
+               """
 
-    # Identify the key assignments between 'wake' and 'sleep'
-    numAssignments = length(inputF) - 2  # Exclude 'wake' and 'sleep'
+    elseif length(assignments) == 3
+        # Parse tree for 3 assignments
+        tokens1 = split(assignments[1], r"\s+")
+        tokens2 = split(assignments[2], r"\s+")
+        tokens3 = split(assignments[3], r"\s+")
+        println("                     [key-assign] ; [assignments]")
+        println("                         /    \\               /    \\")
+        println("                    key $(tokens1[2]) = $(tokens1[4])   [key-assign] ; [assignments]")
+        println("                       /    \\               /    \\          |")
+        println("                    $(tokens1[2]) $(tokens1[4])    key $(tokens2[2]) = $(tokens2[4])   [key-assign]")
+        println("                                             /    \\           /     \\")
+        println("                                        $(tokens2[2]) $(tokens2[4])    key $(tokens3[2]) = $(tokens3[4])")
+        println("                                                              /     \\")
+        println("                                                         $(tokens3[2]) $(tokens3[4])")
 
-    # Define top part structure
-    top = ["[program]", "wake", "[assignments]", "sleep", "[key-assign]", "[x]", "[y]"]
-
-    if numAssignments == 1
-        # Tree for 1 key
-        println("                    $(top[1])")
-        println("             /           |        \\      ")
-        println("           $(top[2])   $(top[3])  $(top[4])")
-        println("                         |")
-        println("                    $(top[5])")
-        println("                        /    \\      ")
-        println("                  key $(top[6]) = $(top[7])")
-
-        # Extract key assignment details
-        assignment = split(inputF[2])  # second element after 'wake'
-        println("                       |     |")
-        println("                       $(assignment[2])   $(chop(assignment[4]))")
-
-    elseif numAssignments == 2
-        # Tree for 2 keys
-        topPart()
-        println("                     $(top[5]) ; $(top[3])")
-        println("                         /   \\           \\")
-        println("                   key $(top[6]) = $(top[7])      $(top[5]) ")
-        println("                        |     |          /   \\")
-
-        # Extract first key assignment details
-        assignment1 = split(inputF[2])
-        println("                        $(assignment1[2])   $(chop(assignment1[4]))   key $(top[6]) = $(top[7])")
-
-        # Extract second key assignment details
-        assignment2 = split(inputF[3])
-        println("                                        |     |")
-        println("                                        $(assignment2[2])    $(chop(assignment2[4]))  ")
-
-    elseif numAssignments == 3
-        # Tree for 3 keys
-        topPart()
-        println("                     $(top[5]) ; $(top[3])")
-        println("                        /   \\                /     \\")
-        println("                  key $(top[6]) = $(top[7])      $(top[5]) ; $(top[3]) ")
-        println("                       |     |           /   \\             \\")
-
-        # Extract first key assignment details
-        assignment1 = split(inputF[2])
-        println("                       $(assignment1[2])   $(chop(assignment1[4]))   key $(top[6]) = $(top[7])      $(top[5])")
-
-        # Extract second key assignment details
-        assignment2 = split(inputF[3])
-        println("                                        |    |            /   \\")
-        println("                                        $(assignment2[2])   $(chop(assignment2[4]))    key $(top[6]) = $(top[7])")
-
-        # Extract third key assignment details
-        assignment3 = split(inputF[4])
-        println("                                                         |     |")
-        println("                                                         $(assignment3[2])    $(chop(assignment3[4])) ")
-
-    elseif numAssignments == 4
-        # Tree for 4 keys
-        topPart()
-        println("                    $(top[5])  ;  $(top[3])")
-        println("                        /   \\               /     \\")
-        println("                  key $(top[6]) = $(top[7])    $(top[5])  ;  $(top[3]) ")
-        println("                       |     |           /   \\         /        \\")
-
-        # Extract first key assignment details
-        assignment1 = split(inputF[2])
-        println("                       $(assignment1[2])   $(chop(assignment1[4]))   key $(top[6]) = $(top[7])  $(top[5]) ; $(top[3]) ")
-
-        # Extract second key assignment details
-        assignment2 = split(inputF[3])
-        println("                                        |     |         /   \\           \\")
-        println("                                        $(assignment2[2])   $(chop(assignment2[4]))  key $(top[6]) = $(top[7])      $(top[5])")
-
-        # Extract third key assignment details
-        assignment3 = split(inputF[4])
-        println("                                                       |     |           /    \\")
-        println("                                                       $(assignment3[2])    $(chop(assignment3[4]))   key $(top[6]) = $(top[7])")
-
-        # Extract fourth key assignment details
-        assignment4 = split(inputF[5])
-        println("                                                                        |     |")
-        println("                                                                        $(assignment4[2])    $(chop(assignment4[4]))")
+    elseif length(assignments) == 4
+        # Parse tree for 4 assignments
+        tokens1 = split(assignments[1], r"\s+")
+        tokens2 = split(assignments[2], r"\s+")
+        tokens3 = split(assignments[3], r"\s+")
+        tokens4 = split(assignments[4], r"\s+")
+        println("                     [key-assign] ; [assignments]")
+        println("                         /    \\               /    \\")
+        println("                    key $(tokens1[2]) = $(tokens1[4])   [key-assign] ; [assignments]")
+        println("                       /    \\               /    \\          /     \\")
+        println("                    $(tokens1[2]) $(tokens1[4])    key $(tokens2[2]) = $(tokens2[4])   [key-assign] ; [assignments]")
+        println("                                             /    \\          /    \\            |")
+        println("                                        $(tokens2[2]) $(tokens2[4])   key $(tokens3[2]) = $(tokens3[4])   [key-assign]")
+        println("                                                              /    \\           /     \\")
+        println("                                                         $(tokens3[2]) $(tokens3[4])   key $(tokens4[2]) = $(tokens4[4])")
+        println("                                                                            /     \\")
+        println("                                                                       $(tokens4[2]) $(tokens4[4])")
     end
 end
