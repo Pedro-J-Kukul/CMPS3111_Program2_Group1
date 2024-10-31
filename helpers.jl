1af95f90b22a1d96917491a9288e4a00ef09e252
Pedro-Kukul <pedro.j.kukul@gmail.com>
2024-10-31 00:23:13 -0600

    Documentation
    refactored updateDerivaitonSteps(state::DerivationState)

--- helpers.jl
+++ helpers.jl
@@ -1,45 +1,43 @@
 include("definitions.jl")
 
-
-function colorize(char::String, fgColor::Symbol, bgColor::Symbol)
-    fgColorValue = getfield(systemConstants.foreground, Symbol(fgColor))
-    bgColorValue = getfield(systemConstants.background, Symbol(bgColor))
-    return string(bgColorValue, fgColorValue, char, systemConstants.foreground.reset, systemConstants.background.reset)
+# Colors a string
+function colorize(text::String, fgColor::Symbol, bgColor::Symbol)
+    fgColorValue = getfield(systemConstants.foreground, Symbol(fgColor)) # gets the foreground colors that matches the symbol
+    bgColorValue = getfield(systemConstants.background, Symbol(bgColor)) # gets the background colors that matches the symbol
+    return string(bgColorValue, fgColorValue, text, systemConstants.foreground.reset, systemConstants.background.reset)
 end
 
+# Adds padding to an Set
 function format_rule(rule::Vector{String}, paddingLengths::Vector{Integer})
     return join([rpad(item, paddingLengths[i]) for (i, item) in enumerate(rule)], "")
 end
 
+# Used to display the current derivation state
 function updateDerivationSteps(state::DerivationState)
-    if isempty(state.derivationStep)
-        state.derivationStep = state.derivationGrammar
-        formattedRule = format_rule(["[program]", "➝", state.derivationStep], systemConstants.derivationPadding)
+    if isempty(state.derivationStep) # check if its a new derivation
+        state.derivationStep = "wake [assignment-list] sleep" # if a new derivation, start from the highest level
+        formattedRule = format_rule(["[program]", "➝", state.derivationStep], systemConstants.derivationPadding) # formmated line
+    elseif occursin(state.derivationReplaceGrammar, state.derivationStep) #checks if the correct text was added correctly
+        state.derivationCounter += 1 # updates the counter 
+        state.derivationStep = replace(state.derivationStep, state.derivationReplaceGrammar => state.derivationGrammar) # replaces the grammar to replace with the new target that was declared in each function
+
+        formattedRule = format_rule([string(state.derivationCounter), "➝", state.derivationStep], systemConstants.derivationPadding) # formats this new line for printing
     else
-        lastStep = split(state.derivationStep, '\n') |> last
-
-        if occursin(state.derivationReplaceGrammar, lastStep)
-            state.derivationCounter += 1
-            state.derivationStep = replace(lastStep, state.derivationReplaceGrammar => state.derivationGrammar)
-
-            formattedRule = format_rule([string(state.derivationCounter), "➝", state.derivationStep], systemConstants.derivationPadding)
-        else
-            throw("Derivation error: target not found")
-        end
+        throw("Derivation error: target not found") #safety measures
     end
-
-    println(colorize(formattedRule, :blue, :white))
+    println(colorize(formattedRule, :blue, :white)) # prints it with colors
 end
 
+# Displays BNF Grammar
 function displayGrammar()
-    border = colorize("#", :magenta, :yellow)^150
+    border = colorize("#", :magenta, :yellow)^150 # line specifications
 
-    println(border)
-    println(colorize("Grammar Rules:", :blue, :green))
-    println(border)
+    println(border) # border
+    println(colorize("Grammar Rules:", :blue, :green)) # Header
+    println(border) # border
 
-    for rule in systemConstants.grammar_rules
+    for rule in systemConstants.grammar_rules# Prints each grammar rule with padding and colors
         println(colorize(format_rule([rule.lhs, rule.arrow, rule.separator, rule.rhs], systemConstants.grammarPadding), :blue, :green))
     end
-    println(border)
+    println(border) # border
 end
